b90b2d39b9ab69dde89bb8de0dea61e378ace51c
rahiji9 <aliahmedrahi007@gmail.com>
2024-12-05 12:44:07 +0600

    change code

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,19 @@
+// /** @type {import('tailwindcss').Config} */
+// export default {
+//   content: [
+//     "./index.html",
+//     "./src/**/*.{js,ts,jsx,tsx}",
+//   ],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [
+//     require('daisyui'),
+//   ],
+// }
 /** @type {import('tailwindcss').Config} */
+import daisyui from 'daisyui';
+
 export default {
   content: [
     "./index.html",
@@ -8,7 +23,6 @@ export default {
     extend: {},
   },
   plugins: [
-    require('daisyui'),
+    daisyui,
   ],
-}
-
+};
