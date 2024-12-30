7b70990b4abc27b3f8d236b7da30b71b53ee2f2d
rahiji9 <aliahmedrahi007@gmail.com>
2024-12-30 22:07:37 +0600

    update css styel

--- src/components/Home/Skills.jsx
+++ src/components/Home/Skills.jsx
@@ -8,6 +8,12 @@ import img8 from "../../assets/skill/mongo-svgrepo-com.svg";
 import img9 from "../../assets/skill/firebase-svgrepo-com.svg";
 import img10 from "../../assets/skill/git-icon-2048x2048-juzdf1l5.png";
 import img11 from "../../assets/skill/github.png";
+import img12 from "../../assets/skill/vercel.png"
+import img13 from "../../assets/skill/1657421703592.png"
+import img14 from "../../assets/skill/nextjs.png"
+import img15 from "../../assets/skill/tailwind.svg"
+import img16 from "../../assets/skill/favicon-192.png"
+
 
 const Skills = () => {
   return (
@@ -21,25 +27,25 @@ const Skills = () => {
         <div className="grid grid-cols-2 lg:grid-cols-3 gap-5 lg:gap-52 ">
           <img
             data-aos="fade-up"
-            className="border w-40 h-40 p-10 hover:bg-white rounded-lg"
+            className="border w-40 h-40 p-10 hover:border-orange-600 hover:border-4 rounded-lg"
             src={img}
             alt=""
           />
           <img
             data-aos="fade-down"
-            className="border w-40 h-40 p-10 hover:bg-white rounded-lg"
+            className="border w-40 h-40 p-10 hover:border-blue-400 hover:border-4 rounded-lg"
             src={img2}
             alt=""
           />
           <img
             data-aos="fade-up"
-            className="border w-40 h-40 p-10 hover:bg-white rounded-lg"
+            className="border w-40 h-40 p-10 hover:border-yellow-500 hover:border-4 rounded-lg"
             src={img3}
             alt=""
           />
           <img
             data-aos="fade-down"
-            className="border w-40 h-40 p-10 hover:bg-white rounded-lg lg:hidden" // for screen only this one is hidden for lg
+            className="border w-40 h-40 p-10 hover:border-cyan-400 hover:border-4 rounded-lg lg:hidden" // for screen only this one is hidden for lg
             src={img5}
             alt=""
           />
@@ -49,25 +55,25 @@ const Skills = () => {
         <div className="grid grid-cols-2 lg:grid-cols-4 gap-5 lg:gap-52 ">
           <img
             data-aos="fade-down"
-            className="border w-40 h-40 p-10 hover:bg-white rounded-lg"
+            className="border w-40 h-40 p-10 hover:border-cyan-400 hover:border-4 rounded-lg"
             src={img5}
             alt=""
           />
           <img
             data-aos="fade-up"
-            className="border w-40 h-40 p-10 hover:bg-white rounded-lg"
+            className="border w-40 h-40 p-10 hover:border-green-500 hover:border-4 rounded-lg"
             src={img6}
             alt=""
           />
           <img
             data-aos="fade-down"
-            className="border w-40 h-40 p-10 hover:bg-white rounded-lg"
+            className="border w-40 h-40 p-10 hover:border-slate-600 hover:border-4 rounded-lg"
             src={img7}
             alt=""
           />
           <img
             data-aos="fade-up"
-            className="border w-40 h-40 p-10 hover:bg-white rounded-lg"
+            className="border w-40 h-40 p-10 hover:border-green-600 hover:border-4 rounded-lg"
             src={img8}
             alt=""
           />
@@ -77,20 +83,54 @@ const Skills = () => {
         <div className="grid grid-cols-2 lg:grid-cols-3 gap-5 lg:gap-52 ">
           <img
             data-aos="fade-down"
-            className="border w-40 h-40 p-10 hover:bg-white rounded-lg"
+            className="border w-40 h-40 p-10 hover:border-yellow-400 hover:border-4 rounded-lg"
             src={img9}
             alt=""
           />
           <img
             data-aos="fade-up"
-            className="border w-40 h-40 p-10 hover:bg-white rounded-lg"
+            className="border w-40 h-40 p-10 hover:border-orange-600 hover:border-4 rounded-lg"
             src={img10}
             alt=""
           />
           <img
             data-aos="fade-down"
-            className="border w-40 h-40 p-10 hover:bg-white rounded-lg"
+            className="border w-40 h-40 p-10 hover:border-4 rounded-lg"
             src={img11}
+            alt=""
+          />
+          <img
+            data-aos="fade-down"
+            className="border w-40 h-40 p-10 hover:border-slate-600 hover:border-4 rounded-lg lg:hidden" // for screen only this one is hidden for lg
+            src={img16}
+            alt=""
+          />
+        </div>
+      </div>
+      <div className="flex justify-center mt-10">
+        <div className="grid grid-cols-2 lg:grid-cols-4 gap-5 lg:gap-52 ">
+          <img
+            data-aos="fade-down"
+            className="border w-40 h-40 p-10 hover:border-slate-600 hover:border-4 rounded-lg"
+            src={img12}
+            alt=""
+          />
+          <img
+            data-aos="fade-up"
+            className="border w-40 h-40 p-10 hover:border-purple-700 hover:border-4 rounded-lg"
+            src={img13}
+            alt=""
+          />
+          <img
+            data-aos="fade-down"
+            className="border w-40 h-40 p-10 hover:border-slate-600 hover:border-4 rounded-lg"
+            src={img14}
+            alt=""
+          />
+          <img
+            data-aos="fade-up"
+            className="border w-40 h-40 p-10 hover:border-blue-400 hover:border-4 rounded-lg"
+            src={img15}
             alt=""
           />
         </div>
